98ae5ecb3a7604f41ce09bc51d67a13d9f65b2c1
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-23 20:33:01 +0530

    adding UC6

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Security.Cryptography.X509Certificates;
@@ -12,10 +13,14 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to ReverseNumber UC4");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Welcome to Stopwatch UC6");
             Console.WriteLine("Please enter the Number to make Reverse ");
            int x = Convert.ToInt32(Console.ReadLine());
            ReverseNumber(x);
+            stopwatch.Stop();
+            Console.WriteLine("Elapeased time  {0} ", stopwatch.ElapsedMilliseconds);
 
         }
         public static int ReverseNumber(int number)
@@ -28,8 +33,11 @@ namespace LogicalPrograms
                 number = number / 10;
             }
             Console.WriteLine(reverse);
+            
             return reverse;
+
         }
     }
+           
     
 }
